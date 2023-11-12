8a65a64a0245034811a40f64c6ae8f1c5e909343
Ash <01tripster@gmail.com>
2023-11-12 21:47:31 +0530

    feat: helper function to fetch github data + type definitions

---
+++ app/lib/data.ts
@@ -0,0 +1,50 @@
+import { Project } from "./definitions";
+
+export async function fetchProjectWithSkills(
+	skill: string
+): Promise<Project[]> {
+	const url = `https://api.github.com/search/repositories?q=topic:${skill}+user:aakashrawat04&order=desc`;
+	const response = await fetch(url, { next: { revalidate: 3600 } });
+	const data: {
+		items: Project[];
+	} = await response.json();
+
+	if (!data || !data.items || !Array.isArray(data.items)) {
+		return [];
+	}
+
+	const filteredProjects = data.items
+		.filter((project: Project) => {
+			const topics = project.topics || [];
+			return topics.includes(skill);
+		})
+		.map((project: Project) => ({
+			name: project.name,
+			topics: project.topics,
+			description: project.description,
+			html_url: project.html_url,
+			homepage: project.homepage,
+		}));
+
+	return filteredProjects;
+}
+
+export async function fetchProjects(username: string) {
+	const url = `https://api.github.com/users/${username}/repos`;
+	const response = await fetch(url, { next: { revalidate: 3600 } });
+	const data = await response.json();
+
+	if (!data || !data.items || !Array.isArray(data.items)) {
+		return [];
+	}
+
+	const projects = data.items.map((project: Project) => ({
+		name: project.name,
+		topics: project.topics,
+		description: project.description,
+		html_url: project.html_url,
+		homepage: project.homepage,
+	}));
+
+	return projects;
+}
---
+++ app/lib/definitions.ts
@@ -0,0 +1,7 @@
+export type Project = {
+	name: string;
+	topics: string[];
+	description: string;
+	html_url: string;
+	homepage: string;
+};

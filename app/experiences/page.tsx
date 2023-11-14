cdad7272e8d6d78e6589c8d943564616bb150613
Ash <01tripster@gmail.com>
2023-11-14 14:56:57 +0530

    feat: update navlinks + navbar

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,4 +1,4 @@
-export default function Contact() {
+export default function Experiences() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
 			<main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
--- app/ui/navLinks.tsx
+++ app/ui/navLinks.tsx
@@ -4,9 +4,8 @@ import { usePathname } from "next/navigation";
 
 const links = [
 	{ name: "Home", path: "/" },
-	{ name: "About", path: "/about" },
 	{ name: "Projects", path: "/projects" },
-	{ name: "Contact", path: "/contact" },
+	{ name: "Experiences", path: "/experiences" },
 ];
 
 export default function NavLinks() {

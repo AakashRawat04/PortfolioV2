6d15fa50c38c98f57e3a981512d49081ef26b5af
Ash <01tripster@gmail.com>
2023-11-14 15:33:32 +0530

    feat: customize scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={poppins.className}>
+			<body className={`${poppins.className} overflow-y-scroll no-scrollbar`}>
 				<Navbar />
 				{children}
 				<Footer />
